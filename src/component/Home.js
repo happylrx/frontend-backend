c488714e64b81a71bfe3f36dfefedf5cc6d980f6
yewenxiang <yewenxiang23@gmail.com>
2017-03-20 13:38:45 +0800

    add

--- src/component/Home.js
+++ src/component/Home.js
@@ -24,16 +24,27 @@ class Home extends React.Component {
     handleInput = (e) => {
         this.setState({addCategory: e.target.value})
     }
+    handleDelete(id){
+      axios.delete(`http://api.duopingshidai.com/category?id=${id}`)
+        .then(res=>
+          {
+            let index = this.state.category.findIndex(item=>item._id===id);
+            this.state.category.splice(index,1);
+            this.setState({data:this.state.category});
+          }
+        )
+    }
     render() {
 
         return (
             <div className="category">
                 <span>商品分类：</span>
-                {this.state.category.map(item =>< a href = "#" key = {
-                    Math.random()
-                } > {
-                    item.name
-                } < /a>)}
+                {this.state.category.map(item =>
+                  <div key = {Math.random()}>
+                    <a href = "#" key = {Math.random()}> {item.name} </a>
+                    <button onClick={this.handleDelete.bind(this,item._id)}>删除</button>
+                  </div>
+              )}
                 <form onSubmit={this.handleForm}>
                     <input type="text" onChange={this.handleInput}/>
                     <input type="submit"/>
