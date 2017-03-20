b0c4844b262a80d8facaf1c2bd5edb99a85cd91e
yewenxiang <yewenxiang23@gmail.com>
2017-03-20 10:28:34 +0800

    add

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,28 +2,42 @@ import React from 'react';
 import axios from 'axios';
 
 class Home extends React.Component {
-    constructor(){
-      super();
-      this.state = {
-        category:[]
-      }
+    constructor() {
+        super();
+        this.state = {
+            category: [],
+            addCategory: ''
+        }
     }
-    componentWillMount(){
-      axios.get("http://api.duopingshidai.com/category")
-        .then((res)=>this.setState({category:res.data.categories}))
-        .catch(err=>console.log(err))
+    getCategory() {
+        axios.get("http://api.duopingshidai.com/category").then((res) => this.setState({category: res.data.categories})).catch(err => console.log(err))
+    }
+    componentWillMount() {
+        this.getCategory();
+    }
+    handleForm = (e) => {
+        e.preventDefault();
+        axios.post("http://api.duopingshidai.com/category", {name: this.state.addCategory}).then(res => {
+            this.getCategory();
+            alert(res.data.msg)});
+    }
+    handleInput = (e) => {
+        this.setState({addCategory: e.target.value})
     }
     render() {
 
         return (
             <div className="category">
-              <span>商品分类：</span>
-              {
-                this.state.category.map(item=><a href="#" key={Math.random()}>{item.name}</a>)
-              }
-              <form onSubmit={this.handleForm}>
-
-              </form>
+                <span>商品分类：</span>
+                {this.state.category.map(item =>< a href = "#" key = {
+                    Math.random()
+                } > {
+                    item.name
+                } < /a>)}
+                <form onSubmit={this.handleForm}>
+                    <input type="text" onChange={this.handleInput}/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
